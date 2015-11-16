99af2c7df0e9c983855ab66e1e173ae22a8f0ca1
Jason Kung <lovepage1212@gmail.com>
2015-11-16 17:56:01 +0800

    Show author datetime

--- commit.py
+++ commit.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import datetime
 from git import git
 
 
@@ -9,8 +9,8 @@ class Commit:
         self.subject = git(['show', '-s', '--format=%s', hash])
         self.message = git(['show', '-s', '--format=%b', hash])
         self.author_date = git(['show', '-s', '--format=%at', hash])
-        self.author_date = date.fromtimestamp(int(self.author_date))
-        self.author_date = self.author_date.strftime("%Y-%m-%d")
+        self.author_date = datetime.fromtimestamp(int(self.author_date))
+        self.author_date = self.author_date.strftime("%Y-%m-%d %H:%M:%S")
         lines = git(['show', '--name-status', '--format=%n', hash]).strip().splitlines()
         self.files = [line.split()[1] for line in lines]
         self.mods = [line.split()[0] for line in lines]
